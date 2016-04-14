5e48de3231ec5d84df471de5d80d25ef4e302f3c
Christian Puchinger <puchinger@playboy.de>
2016-04-14 16:51:44 +0200

    Add translations and translation domain.

---
+++ Resources/HBMDatagridBundle.de.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+  'Keine Einträge gefunden.' => 'Keine Einträge gefunden.',
+  'Sortieren nach:' => 'Sortieren nach:',
+  'Anzahl der Einträge wählen' => 'Anzahl der Einträge wählen',
+  'Einträge anzeigen' => 'Einträge anzeigen',
+  'Einträge' => 'Einträge',
+  'Einträge gesamt' => 'Einträge gesamt',
+  'Menü anzeigen' => 'Menü anzeigen',
+  'bis' => 'bis',
+  'Suchen' => 'Suchen',
+  'Erste Seite' => 'Erste Seite',
+  'Vorherige Seite' => 'Vorherige Seite',
+  'Nächste Seite' => 'Nächste Seite',
+  'Letzte Seite' => 'Letzte Seite',
+  'Erweiterte Felder ein-/ausblenden' => 'Erweiterte Felder ein-/ausblenden',
+  'Weniger' => 'Weniger',
+  'Mehr' => 'Mehr',
+  'Sortierung und Filter zurücksetzen',
+  'Sortierung/Filter zurücksetzen' => 'Sortierung/Filter zurücksetzen',
+  'Reset' => 'Reset',
+];
+
+
+
+
