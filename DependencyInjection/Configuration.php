cd836be876570d524e7861b04fad9f53de0c7d24
Christian Puchinger <puchinger@burda.com>
2021-11-02 23:57:33 +0100

    Upgrade TreeBuilder->getRootNode().

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,12 +18,7 @@ class Configuration implements ConfigurationInterface
   public function getConfigTreeBuilder()
   {
     $treeBuilder = new TreeBuilder('hbm_datagrid');
-
-    if (method_exists($treeBuilder, 'getRootNode')) {
-      $rootNode = $treeBuilder->getRootNode();
-    } else {
-      $rootNode = $treeBuilder->root('hbm_datagrid');
-    }
+    $rootNode = $treeBuilder->getRootNode();
 
     $rootNode
       ->children()
