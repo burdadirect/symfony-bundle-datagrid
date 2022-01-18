b14844317118db1b298977c3535b5ba7395aa0e3
Christian Puchinger <puchinger@burda.com>
2022-01-19 00:24:58 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,8 +15,7 @@ class Configuration implements ConfigurationInterface
   /**
    * {@inheritdoc}
    */
-  public function getConfigTreeBuilder()
-  {
+  public function getConfigTreeBuilder(): TreeBuilder {
     $treeBuilder = new TreeBuilder('hbm_datagrid');
     $rootNode = $treeBuilder->getRootNode();
 
