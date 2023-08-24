55332cb7c19f3f5d8014117fd6cd6e3bc09d2c1d
Joachim Krempel <joachim.krempel@burda.com>
2023-08-24 16:48:19 +0200

    Replace @required with attribute

--- src/Traits/ServiceDependencies/DatagridHelperDependencyTrait.php
+++ src/Traits/ServiceDependencies/DatagridHelperDependencyTrait.php
@@ -3,18 +3,18 @@
 namespace HBM\DatagridBundle\Traits\ServiceDependencies;
 
 use HBM\DatagridBundle\Service\DatagridHelper;
+use Symfony\Contracts\Service\Attribute\Required;
 
 trait DatagridHelperDependencyTrait {
 
   protected DatagridHelper $datagridHelper;
 
   /**
-   * @required
-   *
    * @param DatagridHelper $datagridHelper
    *
    * @return void
    */
+  #[Required]
   public function setDatagridHelper(DatagridHelper $datagridHelper): void {
     $this->datagridHelper = $datagridHelper;
   }
